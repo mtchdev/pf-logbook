4400e616e317a6a9a94ba091ae556c0651a4dc7f
Mitch <me@miitch.io>
2020-07-25 01:04:26 +0800

    Build

---
+++ src/build/precache-manifest.8d6837f376c3a3a0de4e05baad1b2eb1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae754b6f92142b907b0d176e716018bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8e9ad7caa450079dc79",
+    "url": "/static/css/main.503861d9.chunk.css"
+  },
+  {
+    "revision": "4301c69f861c35956a4a",
+    "url": "/static/js/2.9ff95ec7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9ff95ec7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8e9ad7caa450079dc79",
+    "url": "/static/js/main.8bc16206.chunk.js"
+  },
+  {
+    "revision": "dcad35ce53814db73077",
+    "url": "/static/js/runtime-main.a7dc1534.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
