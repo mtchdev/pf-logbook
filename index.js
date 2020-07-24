9fd06cf94b001b877a50bbdbc7986a36c1572420
Michael Greene <mgreene@STUDIO.LOCAL>
2020-07-24 12:31:08 -0400

    Added registration support

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /**
  * projectFLY Logbook Exporter
  * 
- * By: mtchdev & AndrewTech
+ * By: mtchdev & AndrewTech & webdes03
  * 
  * Export your entire projectFLY logbook into a JSON file.
  */
@@ -15,12 +15,47 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-const logs = {}
+const logs = {};
 
 console.log('\n\n--- projectFLY Logbook Exporter ---\n\nGet your authToken from Inspect Element (CTRL+SHIFT+I) on the pF app -> Application Tab -> Local Storage -> authToken\nYou will need to use this token to authenticate the request to get your logbook.\nDO NOT share it with anyone else.\nThe token resets every 10 minutes so be quick!\n\nThis exporter will create a .json file in the current directory with your entire logbook.\n\n\n');
 
 rl.question('authToken (find this in Inspect Element -> Application -> Local Storage) [DO NOT SHARE THIS] right click if ctrl + v doesnt work: ', async (token) => {    
+    
+    let fleet = [];
+    
     try {
+        let res = await axios.get('https://api.projectfly.co.uk/api/v3/fleet/global/users/registrations', {
+            headers: {
+                'Authorization': 'Bearer ' + token
+            }
+        });
+
+        if (res.data) {
+            try {
+                let Objects = Object.values(res.data.data);
+                for (let i = 0; i < Objects.length; i++) {
+                    fleet.push({
+                        "Id": Objects[i].id,
+                        "Registration": Objects[i].registration.registration,
+                        "Icao": Objects[i].registration.aircraft.icao
+                    });
+                }
+                
+                await fs.writeFileSync(`Fleet.json`, JSON.stringify(fleet), 'utf8');
+                console.log('Wrote Fleet.json...');
+            } catch (e) {
+                throw new Error(e);
+            }
+        } else {
+            throw new Error('fuck two');
+        }
+    } catch (e) {
+        throw new Error(e);
+    }
+    
+    try {
+        
+        
         let res = await makeRequest('https://api.projectfly.co.uk/api/v3/bookings/logbook?page=0&filter=', token);
         if (res.data) {
             try {
@@ -32,6 +67,10 @@ rl.question('authToken (find this in Inspect Element -> Application -> Local Sto
                         let res = await makeRequest(`https://api.projectfly.co.uk/api/v3/bookings/${data.id}/details`, token);
                         let log = res.data.data;
                         let flight_number = log.flight_number.split('\/');
+                        let aircraft = 0;
+                        if (log.aircraft && log.aircraft.registration && fleet.filter(fleet => fleet.Registration === log.aircraft.registration.registration).length == 1) {
+                            aircraft = fleet.filter(fleet => fleet.Registration === log.aircraft.registration.registration)[0].Id
+                        }
                         logs[data.id] = {
                             "Booking": {
                                 "Departure_Icao": log.departure.icao,
@@ -39,7 +78,7 @@ rl.question('authToken (find this in Inspect Element -> Application -> Local Sto
                                 "Status": log.status_code,
                                 "Callsign": flight_number[0], 
                                 "Flight_Number": flight_number[1],
-                                "User_Aircraft": 0 // We are unable to get the User_aircraft id
+                                "User_Aircraft": aircraft
                             },
                             "Flight": {
                                 "Time_Started": log.flight.time_started, 
